6fa3850781e4fc918ae2d102ce35b0a253a540d9
Mirey <mireya.marquez44@hotmail.com>
2025-06-22 21:53:55 +0200

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://cnt-bd57fa81-6669-4554-be2d-5fbb001a90d5.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
